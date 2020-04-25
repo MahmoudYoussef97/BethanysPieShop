0d675fe38cf6a3793a450172d3bc6620752cb4e2
MahmoudYoussef97 <Mahmoudyoussef0097@gmail.com>
2020-04-25 17:52:09 +0200

    Final Uploading of BethanysPieShop Using ASP.NET CORE 3

---
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            HomeViewModel homeViewModel = new HomeViewModel() {
+                PiesOfTheWeek = _pieRepository.AllPiesOfTheWeek
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ BethanysPieShop/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{   
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly ShoppingCart _shoppingCart;
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderController(ShoppingCart shoppingCart, IOrderRepository orderRepository)
+        {
+            _shoppingCart = shoppingCart;
+            _orderRepository = orderRepository;
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var shoppingCartItems = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = shoppingCartItems;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+                ModelState.AddModelError("", "Your cart is empty, add some pies first");
+
+            if(ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction(nameof(CheckoutComplete));
+            }
+            return View(order);
+        }
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order. You'll soon enjoy our delicious pies!";
+            return View();
+        }
+    }
+}
